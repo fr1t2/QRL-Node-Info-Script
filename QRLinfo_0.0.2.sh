45d606fee61bc3658bdc09e2e0fa66586cc975da
fr1t2 <fr1t2@protonmail.com>
2019-05-16 19:53:12 -0600

    Version 2

---
+++ QRLinfo_0.0.2.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+
+## QRL Node Information Script
+
+source src/_functions.sh
+
+
+# Variables
+workingDIR=`pwd`;
+
+scriptName=`basename "$0"`;
+hostname=`hostname -A` 2> /dev/null;
+OS=`uname`;
+debianVersion=$(cat /etc/debian_version);
+distro=$(awk -F= 'END { print $2 }' /etc/lsb-release);
+user=`whoami`;
+isUserSudo=$(awk -F':' '{ print $1}' /etc/passwd |grep $user);
+goMinimumVersion=1.10;
+qrlDir=/home/$user/.qrl;
+LSB=/usr/bin/lsb_release
+# This will need to change once we add multiple OS's here...
+AcceptedOS="Ubuntu"
+header QRL_NODE_INFO
+echo -e "Gather information on the local system for diagnostics."
+echo -e "Join our Discord server for help and support\n\t___ https://discord.gg/MSzBSdr ___\n\n"
+# Check for a valid OS for this script to run on.
+
+subHeader Local System Info
+check_os_info;
+_Check=$(echo "$DistributorID" |grep "$AcceptedOS")
+check=$?;
+if [[ "$check" = 1 ]];
+then
+  errHeader Not Running Ubuntu 
+  echo -e "This script has been configured to work with Ubuntu.";
+  printf "%-35s %s\n" "Your OS is:"  "\"$Description\"";
+  echo -e "\nIf you would care to contribute this code is opensource, contributions are welcom!\n";
+  echo -e "https://github.com/fr1t2/QRL_Node_Info"
+  #exit; # Uncomment to exit the script here!
+else
+# Print data to terminal
+  #subHeader " System information "
+  printf "%-35s %s\n" "Your OS is:"  "\"$Description\"";
+  printf "%-35s %s\n" "DistributorID:"  "\"$DistributorID\"";  
+  printf "%-35s %s\n" "Codename:"  "\"$Codename\"";  
+  printf "%-35s %s\n" "Release:"  "\"$Release\""  
+  printf "%-35s %s\n" "Description:"  "\"$Description\""  
+fi
+
+subHeader Host Info
+  check_host_info;
+  printf "%-35s %s\n" "UserName:"  "\"$_UserName\""  
+  printf "%-35s %s\n" "User ID:"  "\"$_UserID\""  
+  IsSudo;
+  # SUDO
+  if [ "$SUDO" = true ];
+  then
+    printf "%-35s %s\n" "User is sudo:"  "\"$SUDO\"";
+  else
+    printf "%-35s %s\n" "User is sudo:"  "\"$SUDO\"";
+  fi  
+    #ROOT 
+    if [ "$ROOT" = true ];
+    then
+      printf "%-35s %s\n" "Script called as root:"  "\"$ROOT\""
+    else
+      printf "%-35s %s\n" "Script called as root:"  "\"$ROOT\""
+    fi    
+  printf "%-35s %s\n" "Hostname:"  "\"$_HostName\"";
+  printf "%-35s %s\n" "ServerTime:"  "\"$_ServerTime\"";
+  printf "%-35s %s\n" "Uptime:"  "\"$_UpTime\"";  
+  printf "%-35s %s\n" "Up Since:"  "\"$_UpSince\"";  
+
+
+# Memory check
+subHeader Server Memory;
+free -th
+
+
+subHeader Network Test;
+# Check network
+if ping -c 1 theqrl.org &> /dev/null;
+  then
+    connection=true;
+    printf "%-35s %s\n" "Can We Reach https://theQRL.org?"  "\"$connection\""  ;
+    # Get known peer and ping
+    if [ -d "$qrlDir" ];
+    then
+      check_qrl_knownpeers; # this sets the variable $qrlPeers to the contents of the peer file.
+      qrlPeersCount=$(echo "$qrlPeers" |wc -l);
+      qrlPeersMath=$(( qrlPeersCount / 2));
+      peerIP=$(echo "$qrlPeers" |sed -n $qrlPeersMath\p |sed 's|[",]||g' | sed 's/.*://')
+      printf "%-35s %s\n" "Peer IP found:"  "\"true\""  ;
+      printf "%-35s %s\n" "Peer's IP:"  "\"$peerIP\""  ;
+      if ping -c 1 $peerIP &> /dev/null;
+      then
+        printf "%-35s %s\n" "PeerIP is reachable"  "\"true\""  ;
+      else
+        printf "%-35s %s\n" "PeerIP is reachable"  "\"false\""  ;
+      fi
+
+    else
+      echo -e "No Peers File Found...";
+    fi
+  else
+    connection=false
+    errHeader Not Connected
+    printf "%-35s %s\n" "Can We Reach https://theQRL.org?"  "\"$connection\"";
+    echo -e "No Connection! Check stuff and come back";
+    exit;
+  fi
+
+
+subHeader QRL Info
+
+## QRL Tests and checks
+
+check_for_pyqrl;
+
+if [ "$py_qrlInstalled" = false ];
+then
+check_for_gqrl;
+fi
+
+if [ "$qrlInstalled" = true ];
+then
+  printf "%-35s %s\n" "QRL Is Installed:"  "\"$qrlInstalled\"" 
+  printf "%-35s %s\n" "QRL Base installed:"  "\"$qrlBase\""        
+
+  if [ "$qrlRuns" = true ];
+  then
+    printf "%-35s %s\n" "QRL Runs:"  "\"$qrlRuns\"";
+    if [ "$qrlBase" = "python" ]
+    then
+      printf "%-35s %s\n" "Python QRL Version:"  "\"$py_qrlVersion\"";
+      printf "%-35s %s\n" "Python QRL PID:"  "\"$py_qrlProcess\"";
+      blockheight=$(qrl state |grep block_height |sed 's/.*://');
+      printf "%-35s %s\n" "blockheight:"  "\"$blockheight\"";
+
+    elif [ "$qrlBase" = go ]; then
+      #printf "%-35s %s\n" "Go QRL Version:"  "\"$go_qrlVersion\"";
+      printf "%-35s %s\n" "Go QRL PID:"  "\"$gqrlProcess\"";
+      blockheight=`curl -s 127.0.0.1:19009/api/GetHeight |jq '.height'`;
+      printf "%-35s %s\n" "blockheight:"  "\"$blockheight\"";
+    fi
+        #statements
+  else
+    printf "%-35s %s\n" "QRL Runs:"  "\"$qrlRuns\"";
+  fi
+
+else
+      printf "%-35s %s\n" "QRL Is Installed:"  "\"$qrlInstalled\""        
+fi
+
+
+# Check for testnet
+if [ -f "$qrlDir/genesis.yml" ]; 
+  then
+    #Genesis file exsists. Set to true and grab contents
+    qrlGenesis=true;
+    qrlGenesisFile=$(cat "$qrlDir/genesis.yml");
+    printf "%-35s %s\n" "Genesis File Found:"  "\"$qrlGenesis\""        
+    
+    if [ "$qrlBase" = "python" ];
+    then
+      check_Testnet=$(qrl state |grep Testnet |sed 's/.*://' | sed 's|[\",]||g');
+      check=$?;
+      if [ "$check" = 0 ]; then
+        qrlTestnet=true;
+        printf "%-35s %s\n" "QRL Testnet:"  "\"$qrlTestnet\""        
+      else
+        qrlTestnet=false;
+        printf "%-35s %s\n" "QRL Testnet:"  "\"$qrlTestnet\""              
+      fi;
+    else
+      echo -e "QRL BASE is not Python, you may be running testnet\nI need to develop this script more..."
+    fi
+  #else
+#    qrlGenesis=false;
+#    qrlTestnet=false;
+#    printf "%-35s %s\n" "QRL Testnet:"  "\"$qrlTestnet\""        
+  fi
+
+
+
+check_qrl_bannedpeers;
+
+if [ "$qrlBannedPeersFound" = true ];
+then
+  printf "%-35s %s\n" "Banned Peers Found:"  "\"$qrlBannedPeersFound\""
+  echo "$qrlBannedPeers"
+#else
+#  printf "%-35s %s\n" "Banned Peers Found:"  "\"$qrlBannedPeersFound\""
+fi
+
+
+check_qrl_wallet;
+
+if [ "$qrlWallet" = true ];
+then
+ printf "%-35s %s\n" "QRL Wallet Found:"  "\"$qrlWallet\""
+ printf "%-35s %s\n" "QRL Wallet Count:"  "\"$qrlWalletCount\""
+ subSubHeader QRL Wallet Location\(s\);
+ echo "$qrlWalletLocation"
+#else
+#   printf "%-35s %s\n" "QRL Wallet Found:"  "\"$qrlWallet\""
+fi
+
+check_qrl_config;
+
+if [ "$qrlConfigSet" = true ];
+then
+  printf "%-35s %s\n" "QRL Config Found:"  "\"$qrlConfigSet\""
+  subSubHeader Config File Contents 
+  echo "$qrlConfig"
+#else
+#    printf "%-35s %s\n" "QRL Config Found:"  "\"$qrlConfigSet\""
+fi
---
+++ src/_functions.sh
@@ -0,0 +1,303 @@
+#!/bin/bash
+
+################################
+#     Formatting Functions     #
+################################
+
+# Header message
+function header(){
+  local h="$@"
+  echo -e "-------------------------------------"
+  echo -e "  ${h}"
+  echo -e "-------------------------------------\n"
+}
+
+# Footer message
+function footer(){
+  local h="$@"
+  echo -e "\n-------------------------------------"
+  echo -e "  ${h}"
+  echo -e "-------------------------------------"
+}
+
+# Sub Header
+function subHeader(){
+  local h="$@"
+  #echo -e "\n###########################################"
+  echo -e "\n#######__ ${h} __#######\n"
+  #echo -e "######################################"
+}
+
+# SubSub Header
+function subSubHeader(){
+local h="$@"
+echo -e "\n#######__ ${h} __#######"
+}
+
+# space out
+function spacer(){
+echo -e "\n**********************************************\n"
+}
+
+# Error Header
+function errHeader(){
+  local h="$@"
+  echo -e "\n**********************************************"
+  echo -e "*** ! ${h} ! ***"
+  echo -e "**********************************************\n"
+}
+
+
+## To Do - assign variables here and later return the data formatted.
+function check_os_info(){
+  # find release info
+  DistributorID=`$LSB -i|sed 's/.*://' |tr -d '\t'`;
+  Codename=`$LSB -c |sed 's/.*://' |tr -d '\t'`;
+  Release=`$LSB -r |sed 's/.*://' |tr -d '\t'`;
+  Description=`$LSB -d |sed 's/.*://' |tr -d '\t'`;
+}
+
+# Get Known Peers list from node
+function check_qrl_knownpeers(){
+  qrlPeersFile=$qrlDir/data/known_peers.json
+  if [ -f "$qrlPeersFile" ];
+  then
+    qrlPeers=$(jq '.[]' $qrlPeersFile)
+  else
+    echo -e "No Peers File Found...";
+  fi
+}
+
+#Get CPU Info and check for AES
+function GetCPU_Info(){
+cpuInfo=`lscpu`
+isAES=$(lscpu |grep aes)
+if [ "$isAES" ];
+then
+  AESenabled=true
+else
+  AESenabled=false;
+fi
+}
+
+#check for jq to format the output of the script. 
+function check_jq() {
+  if  [ -x "$(command -v jq)" ]; 
+    then
+      jqVersion=`jq --version` 2> /dev/null;
+      jqInstalled=true;
+    else
+      jqInstalled=false;
+  fi
+}
+
+# python Check
+function check_py(){
+  if  [ -x "$(command -v python3)" ]; 
+    then 
+      pyVersion=`python3 --version |sed -n 's/.*\([0-9]\.[0-9]\.[0-9]*\).*/\1/p'` 2> /dev/null;
+      pyInstalled=true;
+      if  [ -x "$(command -v pip3)" ];
+      then
+        pip3Version=`pip3 -V |sed -n 's/.*\([0-9]\.[0-9]\.[0-9]*\).*/\1/p'` 2> /dev/null;
+        pip3Installed=true;
+      else
+        pip3Installed=false;       
+      fi
+    else
+      pyInstalled=false;
+  fi
+}
+
+# go Check
+function check_go(){
+  if [ -x "$(command -v go)" ]; 
+   then 
+     #go version
+      goVersion=`go version |sed -n 's/.*\([0-9]\.[0-9][0-9]*\).*/\1/p'` 2> /dev/null;
+      goInstalled=true;
+      if (( $(echo "$goVersion >= $goMinimumVersion" | bc -l) )); 
+      then 
+        goOK=true;
+      else 
+        goOK=false;
+      fi
+    else  
+       goInstalled=false;
+  fi
+}
+
+# Sudo/ROOT check
+function IsSudo(){
+  #check if the user is in the /etc/passwd file, if so we have sudo!
+  if [ "$isUserSudo" ]; 
+    then 
+      SUDO=true;  
+    else 
+      SUDO=false;  
+  fi
+  #Check if the script is run by root?
+  if [ "$EUID" -ne 1000 ];
+    then
+      ROOT=true;
+    else
+      ROOT=false;
+  fi
+}
+
+## To Do - assign variables here and later return the data formatted.
+function check_host_info(){
+ _HostName=$(hostname) ;
+ _UserName=$(whoami) ;
+ _UserID=$(id -u);
+ _ServerTime=$(date) ;
+ _UpTime=$(uptime -p) ;
+ _UpSince=$(uptime -s) ;
+
+# Print verbose information to output  
+  if [ "$verbose" = true ]; 
+  then
+    local dnsips=$(sed -e '/^$/d' /etc/resolv.conf | awk '{if (tolower($1)=="nameserver") print $2}')
+    subHeader "Verbose Net"
+    printf "%-35s %s\n" "FQDN (DNS):"  "  \"$(hostname -f)\""  
+    printf "%-35s %s\n" "DNS domain:"  "  \"$(hostname -d)\""  
+    printf "%-35s %s\n" "Public IPv4:"  "  \"$(curl -s 'icanhazip.com')\""  
+    printf "%-35s %s\n" "Local IP:"  "  \"$(hostname -i)\""  
+    printf "%-35s %s\n" "DNS (DNS IP):"  "  \"$dnsips\""  
+    subHeader end Verbose
+  fi
+}
+
+################################
+# QRL Functions                #
+################################
+
+#Check for go-lang qrl installation
+function check_for_gqrl(){ 
+  if [ -x "$(command -v gqrl)" ]; 
+  then
+    qrlBase="go";
+    gqrlInstalled=true;
+    qrlInstalled=true;
+    #using pgrep to find the process id.
+    if pgrep -x "gqrl" > /dev/null;
+    then
+      gqrlRuns=true;
+      gqrlProcess=`pgrep -x gqrl`;
+      qrlRuns=true;
+    fi;
+  else
+    gqrlInstalled=false;
+    gqrlRuns=false;
+    qrlRuns=false;  
+    qrlInstalled=false;
+  fi;
+}
+# Check for python qrl installation
+function check_for_pyqrl(){
+  if [ -x "$(command -v qrl)" ]; 
+    then
+      qrlBase="python"; 
+      py_qrlInstalled=true;
+      qrlInstalled=true;
+      py_qrlVersion=`qrl --version |sed -n 's/.*\([0-9]\.[0-9][0-9]*\).*/\1/p'`; 
+      if pgrep -x start_qrl > /dev/null;
+        then 
+          py_qrlRuns=true;
+          py_qrlProcess=`pgrep -x start_qrl`;
+          qrlRuns=true;
+        else
+          py_qrlRuns=false;
+        fi;
+    else 
+    py_qrlRuns=false;
+    py_qrlInstalled=false;
+    qrlInstalled=false;
+    qrlRuns=false;
+  fi;
+}
+
+
+
+
+
+#Fix this function to work
+function check_qrl_testnet(){
+    if [ -f "$qrlDir/genesis.yml" ]; 
+  then
+    #Genesis file exsists. Set to true and grab contents
+    qrlGenesis=true;
+    qrlGenesisFile=$(cat "$qrlDir/genesis.yml");
+  else
+    qrlGenesis=false;
+  fi
+
+  if [ "$qrlGenesis" = true ];
+  then
+    check_Testnet=$(qrl state |grep Testnet |sed 's/.*://' | sed 's|[\",]||g');
+    check=$?;
+    if [ "$check" = 0 ]; then
+      qrlTestnet=true;
+    else
+      qrlTestnet=false;
+    fi;
+  else
+    qrlTestnet=false;
+fi;
+}
+
+
+# Check for and print the config file. Only PYTHON currently
+# depends on check_qrl...
+function check_qrl_config(){
+  qrlConfigFile=$qrlDir/config.yml
+  if [ -f "$qrlConfigFile" ];
+  then
+    qrlConfigSet=true;
+    if [ "$qrlBase" = python ]; 
+    then
+      qrlConfig=$(cat $qrlDir/config.yml)
+    else
+      qrlConfigSet=false;
+    fi
+  else
+    qrlConfigSet=false;
+fi
+}
+
+
+
+function check_qrl_bannedpeers(){
+  qrlBannedPeersFile=$qrlDir/banned\_peers.qrl
+  if [ "$qrlBannedPeersFile" = true ];
+  then
+    qrlBannedPeersFound=true;
+    qrlBannedPeers=$(cat $qrlBannedPeersFile |jq .);
+  else
+    qrlBannedPeersFound=false;
+  fi
+}
+
+# qrl wallet Check
+function check_qrl_wallet(){
+    # check for running qrl
+  if [ "$qrlRuns" = true ];
+  then
+    qrlWalletInfo=$(locate -c wallet.json)
+    #check if python version is installed
+    if [ "$qrlWalletInfo" -gt 0 ];
+    then
+      qrlWallet=true;
+      qrlWalletCount=$(locate -c wallet.json);
+      qrlWalletLocation=$(locate wallet.json);
+    else
+      qrlWallet=false;
+      qrlWalletCount=$(locate -c wallet.json);
+    fi;
+  fi;
+}
+
+
+
+
+
